fcacdb4cb6b51778f04126f3b9a41ae32afd2df3
lieyike <672014131@qq.com>
2020-11-19 00:16:55 +0800

    add connteion pool

--- Week_05/jdbcHomeWork/src/main/java/JDBCHomeWork.java
+++ Week_05/jdbcHomeWork/src/main/java/JDBCHomeWork.java
@@ -1,12 +1,17 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.*;
 
 public class JDBCHomeWork {
 
-
+    private static String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/test?serverTimezone=UTC";
+    private static String user = "test";
+    private static String password = "123456";
 
     public static void main(String[] args) {
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://127.0.0.1:3306/test?serverTimezone=UTC", "test", "123456")) {
+        try (Connection conn = DriverManager.getConnection(jdbcUrl
+                , user, password)) {
 
             if (conn != null) {
                 System.out.println("Connected to the database!");
@@ -48,6 +53,40 @@ public class JDBCHomeWork {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(user);
+        config.setPassword(password);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        HikariDataSource dataSource = new HikariDataSource(config);
+
+        try {
+            Connection connection = dataSource.getConnection();
+
+            ResultSet resultSet = connection.createStatement().executeQuery("select * from test");
+
+            while(resultSet.next()) {
+                int id =  resultSet.getInt(1);
+                String name =  resultSet.getString(2);
+                System.out.println("id: " + id + " name: " + name);
+            }
+
+            PreparedStatement deleted = connection.prepareStatement("delete from test where id = '3'");
+            System.out.println("result: " + deleted.executeUpdate());
+
+            PreparedStatement insert = connection.prepareStatement("insert into test values('3', 'C')");
+            System.out.println("result: " + insert.executeUpdate());
+
+            PreparedStatement update = connection.prepareStatement("update test set name = 'D' where id = '3'");
+            System.out.println("result: " + update.executeUpdate());
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
 
     }
---
+++ Week_05/springStarter/src/main/java/io/kimmking/spring01/Student.java
@@ -0,0 +1,34 @@
+package io.kimmking.spring01;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Student implements Serializable {
+    
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void init(){
+        System.out.println("hello...........");
+    }
+    
+    public Student create(){
+        return new Student(101,"KK101");
+    }
+}
