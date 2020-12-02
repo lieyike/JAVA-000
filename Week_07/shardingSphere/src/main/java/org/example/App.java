7a1f6eea468223b571ffb415b2275e3983ece1e0
lieyike <672014131@qq.com>
2020-12-03 01:33:52 +0800

    error on can't find dataSrouce

--- Week_07/shardingSphere/src/main/java/org/example/App.java
+++ Week_07/shardingSphere/src/main/java/org/example/App.java
@@ -1,2 +1,17 @@
-package org.example;public class App {
+package org.example;
+
+import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.util.HashMap;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        File yamlFile = new File(ClassLoader.getSystemResource("config.yaml").getFile());
+        HashMap<String, DataSource> dataSourceHashMap = new HashMap<String, DataSource>();
+        YamlShardingSphereDataSourceFactory.createDataSource(dataSourceHashMap, yamlFile);
+    }
 }
