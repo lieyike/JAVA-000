d653333deb858e340f78ff5cc0056fd6c430a89e
lieyike <672014131@qq.com>
2020-12-03 20:38:08 +0800

    Fix dependency error

--- Week_07/shardingSphere/src/main/java/org/example/App.java
+++ Week_07/shardingSphere/src/main/java/org/example/App.java
@@ -4,14 +4,25 @@ import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFac
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.HashMap;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-
         File yamlFile = new File(ClassLoader.getSystemResource("config.yaml").getFile());
-        HashMap<String, DataSource> dataSourceHashMap = new HashMap<String, DataSource>();
-        YamlShardingSphereDataSourceFactory.createDataSource(dataSourceHashMap, yamlFile);
+        DataSource dataSource =  YamlShardingSphereDataSourceFactory.createDataSource(yamlFile);
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from GoodsTransaction.Goods");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()) {
+            int id = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            System.out.println("id: " + id + " name: " + name );
+        }
+
     }
 }
