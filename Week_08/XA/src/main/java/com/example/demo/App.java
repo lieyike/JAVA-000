3490cf6d35d862fe45ebfb1feb91a025feb7bea5
lieyike <672014131@qq.com>
2020-12-10 00:46:02 +0800

    XA not finish yet

---
+++ Week_08/XA/src/main/java/com/example/demo/App.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.transaction.core.TransactionType;
+import org.apache.shardingsphere.transaction.core.TransactionTypeHolder;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+
+    public static void main(String[] args) throws IOException, SQLException {
+
+        File yamlFile = new File(ClassLoader.getSystemResource("config.yaml").getFile());
+        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(yamlFile);
+        cleanData(dataSource);
+        TransactionTypeHolder.set(TransactionType.XA);
+
+        Connection conn = dataSource.getConnection();
+
+        String sql = "insert into goods values (?, ?);";
+
+        System.out.println("First XA insert successful");
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            conn.setAutoCommit(false);
+            for (int i = 1; i < 11; i++) {
+                statement.setInt(1, i);
+                statement.setString(2, "Name " + i);
+                statement.executeUpdate();
+            }
+            conn.commit();
+            System.out.println("Second XA Start insert data");
+        } catch (Exception e) {
+            System.out.println("Second XA insert failed");
+            conn.rollback();
+        }
+
+
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            conn.setAutoCommit(false);
+            for (int i = 1; i < 11; i++) {
+                statement.setLong(1, i+5);
+                statement.setString(2, "Name " + i);
+                statement.executeUpdate();
+            }
+            conn.commit();
+            System.out.println("Second XA insert successful");
+        } catch (Exception e) {
+            System.out.println("Second XA insert failed");
+            conn.rollback();
+        } finally {
+            conn.close();
+        }
+    }
+
+
+    public static void cleanData(DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.execute("delete from goods");
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
